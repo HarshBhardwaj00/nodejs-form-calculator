1c319011bde468bbb15c727d2a59369b96de5d7d
HarshBhardwaj00 <harshbhara56@gmail.com>
2026-01-30 19:12:59 +0530

    port number fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const requestHandler  = require('./handler');
 
 const x = http.createServer(requestHandler);
 
-const PORT = 3002;
+const PORT = 3003;
 x.listen(PORT,()=>{
   console.log(`server is running on address  http://localhost:${PORT}`);                           
 });
